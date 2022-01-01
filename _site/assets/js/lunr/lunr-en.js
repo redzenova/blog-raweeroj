fc73163c9e6778ca70e8c01f1f7cfaa1c859750f
Raweeroj Thongdee <raweeroj.tho@gmail.com>
2022-01-02 02:50:22 +0700

    first post !

--- _site/assets/js/lunr/lunr-en.js
+++ _site/assets/js/lunr/lunr-en.js
@@ -35,7 +35,7 @@ $(document).ready(function() {
         })
       });
     resultdiv.empty();
-    resultdiv.prepend('<p class="results__found">'+result.length+' Result(s) found</p>');
+    resultdiv.prepend('<p class="results__found">'+result.length+' ผลการค้นหา พบ</p>');
     for (var item in result) {
       var ref = result[item].ref;
       if(store[ref].teaser){
